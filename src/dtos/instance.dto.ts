2d048c1b1d1911e50663fd1bce69e9a69c056da1
Shohel Rana <cse_shohel@yahoo.com>
2024-04-29 11:59:09 +0300

    added model and dto

---
+++ src/dtos/instance.dto.ts
@@ -0,0 +1,24 @@
+export class InstanceDto {
+  instanceId: string
+  name: string
+  planId: string
+  status: string
+  region: string
+  updateDate: Date
+
+  constructor(
+    instanceId: string,
+    name: string,
+    planId: string,
+    status: string,
+    region: string,
+    updateDate: Date
+  ) {
+    this.instanceId = instanceId
+    this.name = name
+    this.planId = planId
+    this.status = status
+    this.region = region
+    this.updateDate = updateDate
+  }
+}
---
+++ src/models/context.model.ts
@@ -0,0 +1,24 @@
+export class Context {
+  name: string
+  crn: string
+  platform: string
+  accountId: string
+  resourceGroupCrn: string
+  targetCrn: string
+
+  constructor(
+    name: string,
+    crn: string,
+    platform: string,
+    accountId: string,
+    resourceGroupCrn: string,
+    targetCrn: string
+  ) {
+    this.name = name
+    this.crn = crn
+    this.platform = platform
+    this.accountId = accountId
+    this.resourceGroupCrn = resourceGroupCrn
+    this.targetCrn = targetCrn
+  }
+}
