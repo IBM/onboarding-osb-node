42f2e40bffe648e4155ade871c4fcbaa9674e9b7
Shohel Rana <cse_shohel@yahoo.com>
2024-06-09 19:46:16 +0300

    test(app): add test case for liveness check

---
+++ .commitlintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'chore',
+        'revert',
+      ],
+    ],
+    'subject-empty': [2, 'never'],
+    'type-empty': [2, 'never'],
+  },
+}
